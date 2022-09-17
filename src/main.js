db1100ecb444bd4eaa22b42911fce6e8428ed5df
Rohan Kiratsata <49232485+rohan-kiratsata@users.noreply.github.com>
2022-09-17 18:26:15 +0530

    js updated - events added

--- src/main.js
+++ src/main.js
@@ -1,24 +1,37 @@
-const currentDate = document.getElementById('currentDate');
-
+const currentDate = document.getElementById("currentDate");
+const eventDateContainer = document.getElementById("event-container");
 
 const getData = async () => {
   var url = "https://history.muffinlabs.com/date";
-
   const response = await fetch(url);
   const allData = await response.json();
-
   // data type - events, death, birth
   var dataType = "Events";
 
   // Object length of specific data
-  var length = Object.keys(allData["data"][dataType]).length;
+  var length = Object.keys(allData["data"]["Events"]).length;
 
-  // random index value of get data
-  var randomIndex = Math.floor(Math.random() * length);
+  currentDate.textContent = allData.date;
 
-  console.log(allData.date);
-  currentDate.textContent = allData.date
+  Object.keys(allData["data"]["Events"])
+    .slice(0, 5)
+    .map((item, index) => {
+      var randomIndex = Math.floor(Math.random() * length);
+      console.log(randomIndex);
 
-  console.log(allData["data"]["Events"][10]);
+      let container = document.createElement("div");
+
+      container.innerHTML = `<div class="flex pt-8 pl-16 gap-x-4">
+    <span class="text-blue-600 year font-semibold  text-2xl" id="event-date">
+      ${allData["data"]["Events"][randomIndex]["year"]}
+    </span>
+    <div >
+      <h3 class="font-semibold">${allData["data"]["Events"][randomIndex]["text"]}</h3>
+      <div class="w-3/5">${allData["data"]["Events"][randomIndex]["html"]}</div>
+    </div>
+  </div>`;
+
+      eventDateContainer.appendChild(container);
+    });
 };
 getData();
