54eb1860caddeef19bd3b72029fc30652d09bf94
Rohan Kiratsata <49232485+rohan-kiratsata@users.noreply.github.com>
2022-09-17 16:26:52 +0530

    js code added

---
+++ src/main.js
@@ -0,0 +1,19 @@
+const getData = async () => {
+  var url = "https://history.muffinlabs.com/date";
+
+  const response = await fetch(url);
+  const allData = await response.json();
+
+  // data type - events, death, birth
+  var dataType = "Events";
+
+  // Object length of specific data
+  var length = Object.keys(allData["data"][dataType]).length;
+
+  // random index value of get data
+  var randomIndex = Math.floor(Math.random() * length);
+
+  console.log(allData.date);
+  console.log(allData["data"]["Events"][10]);
+};
+getData();
