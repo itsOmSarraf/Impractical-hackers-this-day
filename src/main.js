374e0ed1c268d457142b0cc0effae195dc0076e4
Rohan Kiratsata <49232485+rohan-kiratsata@users.noreply.github.com>
2022-09-18 10:53:36 +0530

    design chaanged to card

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ const eventDateContainer = document.getElementById("event-container"); // Event 
 const birthDateContainer = document.getElementById("birth-container"); // Birth container
 const deathDateContainer = document.getElementById("death-container"); // Death container
 
-fetch('https://history.muffinlabs.com/date')
-  .then(res => {
+fetch("https://history.muffinlabs.com/date")
+  .then((res) => {
     return res.json();
   })
-  .then (data => {
+  .then((data) => {
     console.log(data);
-  })
+  });
 
 const getData = async () => {
   var url = "https://history.muffinlabs.com/date";
@@ -26,39 +26,54 @@ const getData = async () => {
 
   currentDate.textContent = allData.date;
 
-
-//************* Add Event Section ************************
+  //************* Add Event Section ************************
 
   Object.keys(allData["data"]["Events"])
-    .slice(0, 5)
+    .slice(0, 6)
     .map((item, index) => {
-
       //  Random Index for event array
       var randomeventIndex = Math.floor(Math.random() * eventLength);
       console.log(randomeventIndex);
 
       let event_container = document.createElement("div");
 
-      event_container.innerHTML = `<div class="flex pt-8 pl-16 gap-x-4">
-    <span class="text-blue-600 year font-semibold  text-2xl" id="event-date">
-      ${allData["data"]["Events"][randomeventIndex]["year"]}
-    </span>
-    <div >
-      <h3 class="font-semibold">${allData["data"]["Events"][randomeventIndex]["text"]}</h3>
-      <div class="w-3/5">${allData["data"]["Events"][randomeventIndex]["html"]}</div>
-    </div>
-  </div>`;
+      //     event_container.innerHTML = `<div class="flex pt-8 pl-16 gap-x-4">
+      //   <span class="text-blue-600 year font-semibold  text-2xl" id="event-date">
+      //     ${allData["data"]["Events"][randomeventIndex]["year"]}
+      //   </span>
+      //   <div >
+      //     <h3 class="font-semibold">${allData["data"]["Events"][randomeventIndex]["text"]}</h3>
+      //     <div class="w-3/5">${allData["data"]["Events"][randomeventIndex]["html"]}</div>
+      //   </div>
+      // </div>`;
+      event_container.innerHTML = `
+  <div class="flex flex-wrap w-[400px] p-5 border border-[#93C5FD] rounded-lg data-card">
+  <!-- date -->
+  <div class="p-2 mb-2 font-semibold text-xl rounded-md bg-[#e46b6b] text-white">
+    <p class="">
+    ${allData["data"]["Events"][randomeventIndex]["year"]}
+    </p>
+  </div>
+  <!-- title -->
+  <div class="text-lg font-medium">
+    <p>${allData["data"]["Events"][randomeventIndex]["text"]}</p>
+  </div>
+  <!-- desc -->
+  <div class="text-md my-2 inline-block">
+  ${allData["data"]["Events"][randomeventIndex]["html"]}
+  </div>
+  <!-- learn more btn -->
+  <a href="" class="p-3 bg-blue-400 rounded-md text-white ">Learn More</a>
+</div>`;
 
       eventDateContainer.appendChild(event_container);
     });
 
+  //*************  Add Birth Section  ************************
 
-    //*************  Add Birth Section  ************************
-
-    Object.keys(allData["data"]["Births"])
+  Object.keys(allData["data"]["Births"])
     .slice(0, 5)
     .map((item, index) => {
-
       //  Random Index for birth array
       var randomBirthIndex = Math.floor(Math.random() * birthLength);
 
@@ -74,16 +89,14 @@ const getData = async () => {
     </div>
   </div>`;
 
-    birthDateContainer.appendChild(birth_container);
-  });
+      birthDateContainer.appendChild(birth_container);
+    });
 
+  //*************  Add Birth Section  ************************
 
-    //*************  Add Birth Section  ************************
-
-    Object.keys(allData["data"]["Deaths"])
+  Object.keys(allData["data"]["Deaths"])
     .slice(0, 5)
     .map((item, index) => {
-
       //  Random Index for death array
       var randomDeathIndex = Math.floor(Math.random() * deathLength);
 
@@ -99,9 +112,8 @@ const getData = async () => {
     </div>
   </div>`;
 
-    deathDateContainer.appendChild(death_container);
-  });
-
+      deathDateContainer.appendChild(death_container);
+    });
 };
 
 //  ************* call the functions ************
